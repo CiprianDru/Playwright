3700443e7ca072273416ae6c84bb750569d26a7a
Ciprian Samarghitean <ciprian.samarghitean@fortech.ro>
2022-09-09 15:03:41 +0300

    project files

---
+++ tests/cart.spec.js
@@ -0,0 +1,6 @@
+const { test, expect } = require('@playwright/test');
+
+
+test('Products can be added to the cart', async ({ page }) => {
+  
+});
---
+++ tests/homepage.spec.js
@@ -0,0 +1,21 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Homepage has the Logo, Search button, user account button in the Header', async ({ page }) => {
+  await page.goto('/foo');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+
+  // create a locator
+  const getStarted = page.locator('text=Get Started');
+
+  // Expect an attribute "to be strictly equal" to the value.
+  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+
+  // Click the get started link.
+  await getStarted.click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
